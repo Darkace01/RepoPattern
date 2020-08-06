9aae2eb20ae0039f813dfb0f2c54cf984a533ba7
Kazeem Quadri <33027659+Darkace01@users.noreply.github.com>
2020-08-06 15:03:11 +0100

    Scarfolded Identity pages

---
+++ RepoPattern/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(RepoPattern.Areas.Identity.IdentityHostingStartup))]
+namespace RepoPattern.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
